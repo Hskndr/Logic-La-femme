3e8aaf3f0bc98e192d7ad1e883126fd7a9e0004c
hiskander aguillon <hiskander@gmail.com>
2019-05-12 01:42:59 -0300

    logic4

--- LaFemme/src/app/member-sign-in/member-sign-in.module.ts
+++ LaFemme/src/app/member-sign-in/member-sign-in.module.ts
@@ -4,9 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MemberSignInComponent } from './member-sign-in/member-sign-in.component';
 import { MemberSignInRoutingModule} from './member-sign-in-routing.module';
 import {FormsModule} from '@angular/forms';
+import { SignInHeaderComponent } from './member-sign-in/sign-in-header/sign-in-header.component';
+import { SignInFooterComponent } from './member-sign-in/sign-in-footer/sign-in-footer.component';
 
 @NgModule({
-  declarations: [MemberSignInComponent],
+  declarations: [MemberSignInComponent, SignInHeaderComponent, SignInFooterComponent],
+  exports: [
+    SignInHeaderComponent
+  ],
   imports: [
     CommonModule,
     MemberSignInRoutingModule,
--- LaFemme/src/app/tell-us-more/tell-us-more.module.ts
+++ LaFemme/src/app/tell-us-more/tell-us-more.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { TellUsMoreRoutingModule } from './tell-us-more-routing.module';
 import { TellUsMoreComponent } from './tell-us-more/tell-us-more.component';
+import {MemberSignInModule} from '../member-sign-in/member-sign-in.module';
 
 @NgModule({
   declarations: [TellUsMoreComponent],
   imports: [
     CommonModule,
-    TellUsMoreRoutingModule
+    TellUsMoreRoutingModule,
+    MemberSignInModule
   ]
 })
 export class TellUsMoreModule { }
